964a328368f79761e187bcdf4c351816967bf499
Vishal Kumar Vasnani <vishalvasnani123@gmail.com>
2022-08-17 12:12:16 -0700

    add LandingPage layout

--- react-frontend/src/Pages/LandingPage/index.js
+++ react-frontend/src/Pages/LandingPage/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../Components/Header';
+import './index.css';
 
 function LandingPage() {
+  const [input, setInput] = useState('');
+
+  const handleTextChange = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
-    <div>
+    <div className="LandingPage-outer-container">
       <Header />
-      Init
+      <div className="LandingPage-container">
+        <div className="LandingPage-input">
+          Input text
+          <textarea onChange={handleTextChange} value={input} className="LandingPage-input-box" />
+        </div>
+        <div className="LandingPage-button-container">
+          <button disabled={input.length === 0} className={input.length === 0 ? 'LandingPage-button-disabled' : 'LandingPage-button'} type="submit">Generate NER</button>
+        </div>
+        <div className="LandingPage-output">Named Entity Recognition</div>
+      </div>
     </div>
   );
 }
